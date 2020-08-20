4aa2422f9646fdd8c0c83976c00eae2ab419640b
AmrDawood-CodeTech <amrdawood91@gmail.com>
2020-08-20 17:21:10 +0200

    Update counterScript.js

--- js/counterScript.js
+++ js/counterScript.js
@@ -2,7 +2,7 @@ let counters =document.querySelectorAll('.statisticsItem span:first-of-type');
 /* console.log(counters);*/ //extract span elements as node list looks like array
 let counterSpeed=100; // the lower the value the faster the counter
 let statisticsClick = document.getElementById('statisticsClick');
-statisticsClick.addEventListener('click',updateCounter);//whem we click on the nav bar element we count
+statisticsClick.addEventListener('click',updateCounter);//when we click on the nav bar element we go to statistics section and count
 
 let statistics = document.getElementById('statistics');
 
@@ -14,8 +14,8 @@ let statisticsBottomPos = statistics.getBoundingClientRect().bottom;//160
 /* The Element.getBoundingClientRect() :
     method returns the size of an element and its position relative to the viewport
     the returned positions : left, top, right, bottom */
-console.log(statisticsTopPos);//statistics section top from viewport position
-console.log(statisticsBottomPos);//statistics section bottom from viewport position
+//console.log(statisticsTopPos);//statistics section top from viewport position
+//console.log(statisticsBottomPos);//statistics section bottom from viewport position
  if(statisticsTopPos <= 350 && statisticsBottomPos >= 160){
     updateCounter()
 } 
